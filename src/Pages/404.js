6961e4cb59b9489e8a2b65bae57d945338eff232
Adeboga Abigail <bogadeji@gmail.com>
2022-02-05 09:49:48 +0100

    Add framework for 404 page

---
+++ src/Pages/404.js
@@ -0,0 +1,13 @@
+import React from "react";;
+
+function NotFound() {
+    return (
+        <div>
+            <h1>The page you are looking for cannot be found</h1>
+            <img src="" alt="" />
+            <a href="/">Go home</a>
+        </div>
+    )
+}
+
+export default NotFound;
