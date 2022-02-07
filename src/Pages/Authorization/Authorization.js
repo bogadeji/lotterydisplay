cb315f1278460c7ae91f93845ab7d7371a61bccc
Adeboga Abigail <bogadeji@gmail.com>
2022-02-07 11:09:32 +0100

    Make login and signup forms responsive

--- src/Pages/Authorization/Authorization.js
+++ src/Pages/Authorization/Authorization.js
@@ -10,12 +10,14 @@ function Authorization() {
     return (
         <div className="authorization grid">
             <div className="authorization-page flow">
-                <div className="logo"><a href="/home"><img src="/logo2.svg" alt="" /></a></div>
-                <p className="fs-700">A few clicks away from creating your Lottery Display</p>
+
+                <div className="logo desktop-logo"><a href="/home"><img src="/logo2.svg" alt="" /></a></div>
+                <div className="logo mobile-logo"><a href="/home"><img src="/logo.svg" alt="" /></a></div>
+                
+                <p className="description">A few clicks away from creating your Lottery Display</p>
                 <img src="/assets/hero.svg" alt="" />
             </div>
             <div className="routes">
-            <div className="logo mobile-logo"><a href="/home"><img src="/logo.svg" alt="" /></a></div>
                 <Routes>
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<Signup />}/>
