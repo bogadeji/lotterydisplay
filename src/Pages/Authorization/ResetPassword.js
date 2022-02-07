6cb0c12f7b1308b8d74f0d0409881d264197e235
Adeboga Abigail <bogadeji@gmail.com>
2022-02-07 12:56:39 +0100

    Reset Password page

--- src/Pages/Authorization/ResetPassword.js
+++ src/Pages/Authorization/ResetPassword.js
@@ -1,27 +1,31 @@
-import React from "react";;
+import React from "react";
+import './ResetPassword.css'
 
 function ResetPassword() {
 
 
     
     return (
-        <div>
-            <h1>Reset Password</h1>
+        <div className="reset-page flow">
+            <h1 className="page-title">Reset Password</h1>
 
-            <form>
-                <h2>Login to your account</h2>
-                <p>Thank you for getting back to Lottery Display, let's access our best recommendation for you</p>
+            <form className="flow form">
+                <h2 className="form-title">Login to your account</h2>
+                <p className="form-heading">Thank you for getting back to Lottery Display, let's access our best recommendation for you</p>
+                <div className="form-inputs flow">
+                    <div className="input-group">
+                        <label>
+                            Username
+                            <input type="text" placeholder="Email or Phone Number" />
+                        </label>
+                    </div>
 
-                <label>
-                    Username
-                    <input type="text" placeholder="Email or Phone Number" />
-                </label>
+                    <button className="reset-password
+                    ">Reset Password</button>
 
-                <input type="submit" value="Reset Password" />
-                <button>Reset Password</button>
-
-                <p>Don't have an account yet? <a href="/auth/signup">Join Lottery Display</a></p>
-                <p>Already have an account? <a href="/auth/login">Login</a></p>
+                    <p>Don't have an account yet? <a href="/auth/signup">Join Lottery Display</a></p>
+                    <p>Already have an account? <a href="/auth/login">Login</a></p>
+                </div>
             </form>
         </div>
     )
